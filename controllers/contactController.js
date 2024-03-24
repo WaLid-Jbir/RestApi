d86579d85d32333f86ccad7abe28e5d008385941
WaLid-Jbir <anawalid737@gmail.com>
2024-03-24 01:16:03 +0000

    Adding UPDATE & DELETE conrtact

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,11 +1,13 @@
 const asyncHandler = require('express-async-handler');
 const Contact = require('../models/contactModel');
 
+// Get all contacts
 module.exports.getContacts = asyncHandler( async (req, res) => {
     const contacts = await Contact.find();
     res.status(200).json(contacts);
 });
 
+// Creata contact
 module.exports.createContact = asyncHandler( async (req, res) => {
     console.log("The request body: ", req.body);
     const {name, email, phone} = req.body;
@@ -21,6 +23,7 @@ module.exports.createContact = asyncHandler( async (req, res) => {
     res.status(201).json(contact);
 });
 
+// Get single contact
 module.exports.getContact = asyncHandler(async (req, res) => {
     const contact = await Contact.findById(req.params.id);
   
@@ -32,10 +35,33 @@ module.exports.getContact = asyncHandler(async (req, res) => {
     res.status(200).json(contact);
 });
 
+// Update contact
 module.exports.updateContact = asyncHandler( async (req, res) => {
-    res.status(200).json({ message: 'Update Contact' });
+    const contact = await Contact.findById(req.params.id);
+  
+    if (!contact) {
+      res.status(404);
+      throw new Error("Contact not found");
+    }
+
+    const updatedContact = await Contact.findByIdAndUpdate(
+        req.params.id,
+        req.body,
+        {new: true}
+    );
+
+    res.status(200).json(updatedContact);
 });
 
+// Delete contact
 module.exports.deleteContact = async (req, res) => {
-    res.status(200).json({ message: 'Delete Contact' });
+    const contact = await Contact.findById(req.params.id);
+  
+    if (!contact) {
+      res.status(404);
+      throw new Error("Contact not found");
+    }
+
+    await contact.deleteOne();
+    res.status(200).json({ message: 'Contact deleted successfully' });
 }
